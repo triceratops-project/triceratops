f65ff6fd65934f19126b15596e6b63836989828e
FelixKLG <felix@felixklg.dev>
2023-11-24 17:19:06 +0000

    more migrations

--- crates/webserver/migration/src/lib.rs
+++ crates/webserver/migration/src/lib.rs
@@ -1,9 +1,11 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20231120_235338_create_users;
-mod m20231122_004821_create_servers;
 mod m20231122_003448_create_nodes;
+mod m20231122_004821_create_servers;
 mod m20231122_135619_create_locations;
+mod m20231124_171543_create_nests;
+mod m20231124_171550_create_eggs;
 
 pub struct Migrator;
 
@@ -12,9 +14,11 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20231120_235338_create_users::Migration),
-            Box::new(m20231122_004821_create_servers::Migration),
             Box::new(m20231122_003448_create_nodes::Migration),
+            Box::new(m20231122_004821_create_servers::Migration),
             Box::new(m20231122_135619_create_locations::Migration),
+            Box::new(m20231124_171543_create_nests::Migration),
+            Box::new(m20231124_171550_create_eggs::Migration),
         ]
     }
 }
