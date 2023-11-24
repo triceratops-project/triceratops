cec54551f468adb7d06147dbf7125edfaf3a2010
FelixKLG <felix@felixklg.dev>
2023-11-24 21:34:48 +0000

    Gen entities

--- crates/webserver/entity/src/locations.rs
+++ crates/webserver/entity/src/locations.rs
@@ -14,6 +14,15 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(has_many = "super::nodes::Entity")]
+    Nodes,
+}
+
+impl Related<super::nodes::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Nodes.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
--- crates/webserver/entity/src/nodes.rs
+++ crates/webserver/entity/src/nodes.rs
@@ -9,9 +9,29 @@ pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub id: String,
     pub name: String,
+    pub description: Option<String>,
+    pub location_id: String,
+    pub connection_address: String,
+    pub secure: bool,
+    pub token: String,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::locations::Entity",
+        from = "Column::LocationId",
+        to = "super::locations::Column::Id",
+        on_update = "Cascade",
+        on_delete = "Cascade"
+    )]
+    Locations,
+}
+
+impl Related<super::locations::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Locations.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
