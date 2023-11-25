2a198c154709f7abf5a6972d258111eea5f67b22
FelixKLG <felix@felixklg.dev>
2023-11-25 13:44:25 +0000

    servers endpoint

--- crates/webserver/src/routes/servers/servers.rs
+++ crates/webserver/src/routes/servers/servers.rs
@@ -1,8 +1,27 @@
-use axum::Json;
-use serde_json::{json, Value};
+use crate::state::AppState;
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use sea_orm::EntityTrait;
+use serde_json::json;
+use triceratops_server_entity::servers as Servers;
 
-pub async fn handler() -> Json<Value> {
-    Json(json!({
-        "servers": [1, 2, 3],
-    }))
+pub async fn handler(State(state): State<AppState>) -> Response {
+    let servers = Servers::Entity::find().all(state.get_pool()).await;
+
+    let servers = match servers {
+        Ok(servers) => servers,
+        Err(_) => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({"message": "Internal Server Error"})),
+            )
+                .into_response();
+        }
+    };
+
+    Json(servers).into_response()
 }
