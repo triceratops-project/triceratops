dfca329308d4d380d97207c7266b5be469d09875
FelixKLG <felix@felixklg.dev>
2023-11-28 13:38:59 +0000

    next static export and cors development patch

--- apps/panel/next.config.js
+++ apps/panel/next.config.js
@@ -2,6 +2,7 @@ const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
         remotePatterns: [
             { // for testing images
--- crates/webserver/src/web_server/mod.rs
+++ crates/webserver/src/web_server/mod.rs
@@ -1,6 +1,8 @@
+use axum::http::{header, Method};
 use dotenvy::dotenv;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
+use tower_http::cors::{AllowOrigin, CorsLayer};
 
 mod middleware;
 mod routes;
@@ -11,8 +13,27 @@ pub async fn start() {
 
     let socket_address = SocketAddr::from(([127, 0, 0, 1], 8080));
 
-    let router = routes::route().await;
+    #[cfg(not(debug_assertions))]
+    compile_error!("REMOVE THE DANGEROUS CORS SHIT, CSRF AND ALL!?");
+
+    let cors = CorsLayer::new()
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PATCH,
+            Method::PUT,
+            Method::DELETE,
+        ])
+        .allow_headers([header::AUTHORIZATION, header::CONTENT_TYPE])
+        .allow_origin(AllowOrigin::mirror_request());
+
+    let router = routes::route().await.layer(cors);
 
     let listener = TcpListener::bind(socket_address).await.unwrap();
-    axum::serve(listener, router.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap();
+    axum::serve(
+        listener,
+        router.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
 }
