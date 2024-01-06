dd7a4591f6d75d441482b9d62e9029639bb8eddb
FelixKLG <felix@felixklg.dev>
2024-01-06 00:46:14 +0000

    404 handler example

--- apps/panel-svelte/src/routes/users/+page.ts
+++ apps/panel-svelte/src/routes/users/+page.ts
@@ -2,6 +2,7 @@ import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async () => {
 	const res = await fetch("/api/users");
+
 	const users = await res.json();
 
 	return { users };
--- apps/panel-svelte/src/routes/users/[id]/+page.ts
+++ apps/panel-svelte/src/routes/users/[id]/+page.ts
@@ -1,9 +1,16 @@
+import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ params }) => {
 	const { id } = params;
 
 	const res = await fetch(`/api/users/${id}`);
+
+	if (res.status == 404) {
+		console.log(404);
+		error(404, "Not found");
+	}
+	
 	const user = await res.json();
 
 	return { user };
