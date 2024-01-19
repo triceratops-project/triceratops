aa198e88f53de1516f4c8a05d2b8ea29e5008da1
FelixKLG <felix@felixklg.dev>
2024-01-19 19:22:58 +0000

    remove unneeded cors crap

--- crates/webserver/src/web_server/mod.rs
+++ crates/webserver/src/web_server/mod.rs
@@ -1,8 +1,6 @@
-use axum::http::{header, Method};
 use dotenvy::dotenv;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
-use tower_http::cors::{AllowOrigin, CorsLayer};
 
 mod middleware;
 mod routes;
@@ -14,21 +12,7 @@ pub async fn start() {
     let socket_port = std::env::var("PORT").expect("PORT must be set");
     let socket_address = SocketAddr::from(([127, 0, 0, 1], socket_port.parse::<u16>().unwrap()));
 
-    #[cfg(not(debug_assertions))]
-    compile_error!("REMOVE THE DANGEROUS CORS SHIT, CSRF AND ALL!?");
-
-    let cors = CorsLayer::new()
-        .allow_methods([
-            Method::GET,
-            Method::POST,
-            Method::PATCH,
-            Method::PUT,
-            Method::DELETE,
-        ])
-        .allow_headers([header::AUTHORIZATION, header::CONTENT_TYPE])
-        .allow_origin(AllowOrigin::mirror_request());
-
-    let router = routes::route().await.layer(cors);
+    let router = routes::route().await;
 
     let listener = TcpListener::bind(socket_address).await.unwrap();
     axum::serve(
