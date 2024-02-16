3ca5be4bca0e043c55471bbefb8d385e72b59dc6
FelixKLG <felix@felixklg.dev>
2024-02-16 17:21:56 +0000

    Housekeep unused middleware

--- crates/webserver/src/web_server/middleware/mod.rs
+++ crates/webserver/src/web_server/middleware/mod.rs
@@ -1,12 +1,9 @@
 mod authenticated;
-// mod rate_limit;
-// mod example;
 mod unauthenticated;
 
 // Function Middleware
 pub use authenticated::auth as Auth;
-// pub use rate_limit::rate_limit as RateLimit;
 pub use unauthenticated::guest as Guest;
 
 // Tower Middleware
-// pub use remote_addr::RemoteAddressLayer;
+// pub use Example::ExampleLayer;
